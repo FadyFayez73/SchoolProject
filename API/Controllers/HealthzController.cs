9e19e262010f4292da9b1195896216ed182c4231
Fady <fadyfayez.nt@gmail.com>
2025-07-15 00:14:42 +0300

    # Add Healthz point

---
+++ API/Controllers/HealthzController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthzController : ControllerBase
+    {
+        /// <summary>
+        /// Health check endpoint
+        /// </summary>
+        /// <returns>Returns a simple OK response</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Service is running");
+        }
+    }
+}
