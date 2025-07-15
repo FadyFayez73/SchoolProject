7b17aaa7ffc4dbbbc975282ba084ef0a813e0371
Fady <fadyfayez.nt@gmail.com>
2025-07-16 00:46:57 +0300

    # Fix Connection String

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+Console.WriteLine("DB Connection: " + builder.Configuration.GetConnectionString("DefaultConnection"));
+
+
 // DbContext configuration
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
